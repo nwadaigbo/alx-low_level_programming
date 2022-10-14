71c627092184e6ab4b79899cacef16cfe5c93fe0
nwadaigbo <damssel2000@yahoo.com>
2022-10-13 17:02:10 -0700

    Intro to C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
 */
 	int main (void)
 {
-	printf ("with proper grammer, but the outcome is a piece of art,\n")
-	return 0
+	printf ("with proper grammer, but the outcome is a piece of art,\n");
+	return 0;
 
 
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - prints the size of various types on the computer it is compilled to run on
- *Return: 0
+ * main - prints the size of various types on
+ * the computer it is compilled to run on
+ * Return: 0
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
