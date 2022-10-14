b39082ae275a502a9c269dbabd8e171922c9f0ef
nwadaigbo <damssel2000@yahoo.com>
2022-10-13 17:30:20 -0700

    Intro on C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Return: displays a message to the terminal.
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 	int main (void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types on
- * the computer it is compilled to run on
- * Return: 0 (Success)
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
+
 
 int main(void)
 {
