4301bb6ce17079d3ceb2a205e07bac60a678c97a
nwadaigbo <damssel2000@yahoo.com>
2022-11-16 07:25:43 -0800

    Function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+
+/**
+ * int_index - entry function
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: sucess 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
+Footer
+© 2022 GitHub, Inc.
